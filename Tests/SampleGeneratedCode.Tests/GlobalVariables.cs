faee20d06595843ec4f0e5457a7bd55281857ac4
Juan Ignacio DAmato <juanidamato@outlook.com>
2023-07-19 17:32:15 -0500

    integration test for repository (copy snapshot)

---
+++ Tests/SampleGeneratedCode.Tests/GlobalVariables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleGeneratedCode.Tests
+{
+    public static class GlobalVariables
+    {
+        public const string SQLCopyBat= "C:\\juani\\backupdb"; //todo
+        public const string SQLLocalDB = "Persist Security Info=True;Initial Catalog=dbTest;Data Source=(LocalDB)\\MSSQLLocalDB;TrustServerCertificate=True";//todo
+    }
+}
--- Tests/SampleGeneratedCode.Tests/Repositories/ProductRepositoryTest.cs
+++ Tests/SampleGeneratedCode.Tests/Repositories/ProductRepositoryTest.cs
@@ -7,6 +7,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CliWrap;
+using SampleGeneratedCodeApplication.Commons.Interfaces.Repositories;
+using SampleGeneratedCodeApplication.Commons.Interfaces.Infrastructure;
+using SampleGeneratedCodeInfrastructure;
+using Microsoft.Extensions.Configuration;
+using SampleGeneratedCodeDomain.Entities;
 
 namespace SampleGeneratedCode.Tests.Repositories
 {
@@ -14,7 +20,27 @@ namespace SampleGeneratedCode.Tests.Repositories
     {
         public ProductRepositoryTest()
         {
-                
+            var result1 = Task.Run(async () =>
+            {
+               var result2 =await CliWrap.Cli.Wrap(Path.Combine(GlobalVariables.SQLCopyBat, "copia.bat"))
+                 .WithWorkingDirectory(GlobalVariables.SQLCopyBat)
+                 .WithValidation(CommandResultValidation.None)
+                 .ExecuteAsync();
+                return result2;
+            }).Result;
+            Console.WriteLine($"Stop-Copy-Start-Result:{result1}");
+        }
+
+        private IConfiguration buildConfig()
+        {
+            var myConfiguration = new Dictionary<string, string?>
+            {
+                {"ConnectionStrings:Default",GlobalVariables.SQLLocalDB}
+            };
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(myConfiguration)
+                .Build();
+            return configuration;
         }
 
         [Fact]
@@ -22,7 +48,35 @@ namespace SampleGeneratedCode.Tests.Repositories
         {
             //arrange
             Mock<ILogger<ProductRepository>> logger = new Mock<ILogger<ProductRepository>>();
+            IConfiguration config = buildConfig();
+            IDatabaseHelper db = new SqlDatabaseHelper(config);
+            IProductRepository sut = new ProductRepository(logger.Object, db);
+            ProductEntity? product;
+            bool bolR;
+            //act
+            (bolR, product) = await sut.GetByIdAsync("prod01");
 
+            //assert
+            Assert.True( bolR);
+            Assert.NotNull(product);
+            Assert.True( string.Compare( "prod01", product.IdProduct,true)==0);
+        }
+        [Fact]
+        public async Task ProductRepository_GetProductById_InValidId_Return_Null()
+        {
+            //arrange
+            Mock<ILogger<ProductRepository>> logger = new Mock<ILogger<ProductRepository>>();
+            IConfiguration config = buildConfig();
+            IDatabaseHelper db = new SqlDatabaseHelper(config);
+            IProductRepository sut = new ProductRepository(logger.Object, db);
+            ProductEntity? product;
+            bool bolR;
+            //act
+            (bolR, product) = await sut.GetByIdAsync("prod01xx");
+
+            //assert
+            Assert.True(bolR);
+            Assert.Null(product);
         }
     }
 }
